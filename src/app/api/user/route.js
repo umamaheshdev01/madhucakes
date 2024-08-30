64a9d8a7efb5cc53a908e66d95120a1cfbf0762b
Sai Kiran Chevuri <vickyrishi2446@gmail.com>
2024-08-31 02:36:27 +0530

    Working

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,33 +1,28 @@
 import { createClient } from '@supabase/supabase-js'
+import { NextResponse } from 'next/server'
+
 const supabaseUrl = 'https://ymjsanlykbfwjrxbvzej.supabase.co'
-const supabaseKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltanNhbmx5a2Jmd2pyeGJ2emVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUwNDU4NzcsImV4cCI6MjA0MDYyMTg3N30.wjk5gH-BJpazZeABuiXxUY8C2WIbRgh8C3soTIq7I0M'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltanNhbmx5a2Jmd2pyeGJ2emVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUwNDU4NzcsImV4cCI6MjA0MDYyMTg3N30.wjk5gH-BJpazZeABuiXxUY8C2WIbRgh8C3soTIq7I0M'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-import { NextResponse } from "next/server"
-
-
-
-export const POST = async(req,res)=>{
-
-    
-    
-    
+export const POST = async (req) => {
+  try {
     const data1 = await req.json()
-
 
     const myemail = data1.data.email_addresses[0].email_address
     const namam = data1.data.username
     const proficpic = data1.data.profile_img_url
 
-    const {data,error} = await supabase.from('Users').insert({email : myemail,username : namam,pic : proficpic})
+    const { data, error } = await supabase
+      .from('Users')
+      .insert({ email: myemail, username: namam, pic: proficpic })
 
-    if(error)
-    {
-        return NextResponse.json({error},{status:500})
-    }
-    else
-    {
-        return NextResponse.json({msg:myemail},{status:200})
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 })
     }
 
+    return NextResponse.json({ msg: myemail }, { status: 200 })
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
 }
