1c6fddc9e9f32a86da22981ee97ea748814d9ca0
Sai Kiran Chevuri <vickyrishi2446@gmail.com>
2024-08-31 01:48:18 +0530

    'ok'

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://hjrsxzuuvihvcxsvuegx.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqcnN4enV1dmlodmN4c3Z1ZWd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0NjE4MDEsImV4cCI6MjAzNDAzNzgwMX0.zpkbfkaqQU0ixqLtOwXJqZm5sSB4wRVILpRDCupiBUM'
+const supabaseUrl = 'https://ymjsanlykbfwjrxbvzej.supabase.co'
+const supabaseKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltanNhbmx5a2Jmd2pyeGJ2emVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUwNDU4NzcsImV4cCI6MjA0MDYyMTg3N30.wjk5gH-BJpazZeABuiXxUY8C2WIbRgh8C3soTIq7I0M'
 const supabase = createClient(supabaseUrl, supabaseKey)
+
 import { NextResponse } from "next/server"
 
 
